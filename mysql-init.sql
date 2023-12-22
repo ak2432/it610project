4eb833686f4f37e9a868736bd5fffe02be644be5
ak2432 <45631191+ak2432@users.noreply.github.com>
2023-12-22 16:56:25 -0500

    Delete mysql-init.sql

--- mysql-init.sql
+++
@@ -1,4 +0,0 @@
--- Grant privileges to create roles and users
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '';
-FLUSH PRIVILEGES;
